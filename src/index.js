abb76659c9f8f4da8be25375ac38bcf263724f25
ZhuravlevAlexei <110524860+ZhuravlevAlexei@users.noreply.github.com>
2023-01-15 14:27:01 +0200

    after styling

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import './css/styles.css';
+import './css/additional-styles.css';
 
 import { fetchCountries } from './js/fetchCountries';
+import {
+  createCountryList,
+  createCountryInfo,
+  clearCountryList,
+  clearCountryInfo,
+} from './js/library.js';
 
 import debounce from 'lodash.debounce';
 const DEBOUNCE_DELAY = 300;
@@ -13,26 +20,34 @@ input.addEventListener('input', debounce(onInput, DEBOUNCE_DELAY));
 
 function onInput(evt) {
   const inputData = evt.target.value.trim();
-  console.log(inputData);
+
   if (!inputData) {
+    clearCountryList();
+    clearCountryInfo();
     return;
   }
   const resp = fetchCountries(inputData)
     .then(data => {
       if (data.length > 10) {
+        clearCountryInfo();
+        clearCountryList();
+
         Notify.info(
           'Too many matches found. Please enter a more specific name.'
         );
       }
       if (data.length >= 2 && data.length <= 10) {
+        clearCountryInfo();
+        createCountryList(data);
       }
       if (data.length === 1) {
-        //1 країна
+        clearCountryList();
+        createCountryInfo(data);
       }
     })
     .catch(err => {
+      clearCountryInfo();
+      clearCountryList();
       Notify.failure('Oops, there is no country with that name');
     });
 }
-
-// console.log(resp);
---
+++ src/js/library.js
@@ -0,0 +1,53 @@
+countryList = document.querySelector('.country-list');
+countryInfo = document.querySelector('.country-info');
+
+export function createCountryList(countries) {
+  console.log(countries);
+  countryList.innerHTML = countries
+    .map(
+      ({ flags: { svg }, name: { common } }) => `      
+      <li class = "js-country-list">
+        <img src="${svg}" alt="State flag" width='24' height='24'>
+        <p class = "js-country-text">${common}</p>
+      </li>`
+    )
+    .join('');
+}
+
+export function createCountryInfo(country) {
+  countryInfo.innerHTML = country
+    .map(
+      ({
+        flags: { svg },
+        name: { common },
+        capital,
+        population,
+        languages,
+      }) => `<div class = "js-country-card">       
+   <img src="${svg}" alt="State flag" width='24' height='24' >
+    <p>${common}</p>
+    </div>
+      <ul class = "js-info-card">
+        <li>
+          <p class = "js-info-header">Capital: <span class ="js-info-text">${capital}</span></p>
+        </li>
+        <li>
+          <p class = "js-info-header">Population: <span class ="js-info-text">${population.toLocaleString(
+            'uk'
+          )}</span></p>
+        </li>
+        <li>
+          <p class = "js-info-header">Languages: <span class ="js-info-text">${Object.values(
+            languages
+          ).join(', ')}</span></p>
+        </li>
+      </ul>`
+    )
+    .join('');
+}
+export function clearCountryList() {
+  countryList.innerHTML = '';
+}
+export function clearCountryInfo() {
+  countryInfo.innerHTML = '';
+}
